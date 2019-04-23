c8a11f4f0135ce192e7a3e474d53cd9e4e6057a2
Matt Insler <mattinsler@users.noreply.github.com>
2019-04-23 01:15:57 -0400

    Add dependency resolution to parsing (#1)
    * add dependencies to parser

--- packages/example/src/server.ts
+++ packages/example/src/server.ts
@@ -1,11 +1,8 @@
 import fs from 'fs';
 import grpc from 'grpc';
-// import path from 'path';
-// import { fromProtos } from '@protos/parser';
 import { toPackageDefinition } from '@protos/to-package-definition';
 
 (async () => {
-  // const protos = await fromProtos(path.resolve(__dirname, '..', 'protos'));
   const protos = require('../protos.json');
   const packageDefinition = toPackageDefinition(protos);
   const root = grpc.loadPackageDefinition(packageDefinition);
@@ -21,4 +18,6 @@ import { toPackageDefinition } from '@protos/to-package-definition';
 
   server.bind('0.0.0.0:50051', grpc.ServerCredentials.createInsecure());
   server.start();
+
+  console.log('Listening on 0.0.0.0:50051');
 })().catch(err => console.log(err.stack));
--- packages/parser/src/index.ts
+++ packages/parser/src/index.ts
@@ -5,14 +5,40 @@ import crypto from 'crypto';
 import { ProtoSpec } from '@protos/core';
 
 import { protoc } from './protoc';
-import { collectFiles } from './protofiles';
+import { collectFiles, getDependencyMap } from './protofiles';
 import { transformRelativeToAbsoluteImports } from './transform';
 
+function merge(specs: ProtoSpec[]): ProtoSpec {
+  const combined: ProtoSpec = { enums: [], messages: [], services: [] };
+
+  for (let spec of specs) {
+    combined.enums.push(...spec.enums);
+    combined.messages.push(...spec.messages);
+    combined.services.push(...spec.services);
+  }
+
+  return combined;
+}
+
 export async function fromProtos(protodir: string, opts: { includes?: string[] } = {}): Promise<ProtoSpec> {
+  const { includes = [] } = opts;
+
   const tmpdir = path.resolve(os.tmpdir(), crypto.randomBytes(16).toString('hex'));
   await fs.copy(protodir, tmpdir);
 
-  const files = await collectFiles(tmpdir);
-  await transformRelativeToAbsoluteImports(files);
-  return protoc(files.map(f => path.resolve(f.absoluteDir, f.filename)), [tmpdir, ...(opts.includes || [])]);
+  const protofiles = await collectFiles(tmpdir);
+  await transformRelativeToAbsoluteImports(protofiles);
+
+  const files = protofiles.map(f => path.resolve(f.absoluteDir, f.filename));
+  const depfiles = Object.keys(await getDependencyMap(files, [tmpdir, ...includes])).filter(f => !f.startsWith(tmpdir));
+
+  return merge([await protoc(files, [tmpdir, ...includes]), await protoc(depfiles, includes)]);
+}
+
+export async function fromProtoFiles(files: string[], opts: { includes?: string[] } = {}): Promise<ProtoSpec> {
+  const { includes = [] } = opts;
+  const fileSet = new Set(files);
+  const depfiles = Object.keys(await getDependencyMap(files, includes)).filter(f => !fileSet.has(f));
+
+  return merge([await protoc(files, includes), await protoc(depfiles, includes)]);
 }
